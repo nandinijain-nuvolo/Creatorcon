98eba468ba6398aadf9b0783060d69677299708b
Nandini Jain <nandinijain2410@gmail.com>
2021-05-07 21:23:18 +0530

    Added listener on space bar press.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect,useCallback,useState} from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
@@ -6,27 +6,46 @@ import { SayButton } from "react-say";
 
 const App = () => {
   const { transcript, resetTranscript } = useSpeechRecognition();
+  const [title,setTitle] = useState("Hold space bar to speak.")
+  const startListen = useCallback((event) => {
+    if(event.keyCode === 32) {
+      SpeechRecognition.startListening({ continuous: true });
+      setTitle("Listening...");
+    }
+  }, []);
+  const stopListen = useCallback((event) => {
+    if(event.keyCode === 32) {
+      SpeechRecognition.stopListening();
+      setTitle("Hold space bar to speak.");
+    }
+  }, []);
+  useEffect(() => {
+    document.addEventListener("keydown", startListen, false);
+    document.addEventListener("keyup", stopListen, false);
 
+    return () => {
+      document.removeEventListener("keydown", startListen, false);
+      document.addEventListener("keyup", stopListen, false);
+    };
+  }, [startListen, stopListen]);
+
+  
   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
     return null;
   }
+
   return (
     <div>
-      <button
-        onClick={() => SpeechRecognition.startListening({ continuous: true })}
-      >
-        Start
-      </button>
-      <button onClick={() => SpeechRecognition.stopListening()}>Stop</button>
+      <h3>{title}</h3>
       <button onClick={() => resetTranscript()}>Reset</button>
       <p>{transcript}</p>
 
-      <SayButton
+     {transcript &&  <SayButton
         onClick={(event: any) => console.log(event)}
         speak={transcript}
       >
-      Play
-      </SayButton>
+      Click to play your recording!
+      </SayButton>}
     </div>
   );
 };
